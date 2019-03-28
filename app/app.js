fd455fbc37b8d7291c44021882c6d9d5ec8e689e
krzysiek <janekjanecki14@gmail.com>
2019-03-28 12:46:03 +0100

    Merge

--- app/app.js
+++ app/app.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const SyllabusController = require('./controllers/syllabusController');
-<<<<<<< HEAD
-// const GeneratorController = require('./controllers/generatorController');
-=======
->>>>>>> 6923fbb780da154f46e5103d51dab9dbc55ea450
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -15,10 +11,6 @@ app.options('*', cors());
 
 // Routes
 app.use('/syllabus', SyllabusController);
-<<<<<<< HEAD
-// app.use('/generator', GeneratorController);
-=======
->>>>>>> 6923fbb780da154f46e5103d51dab9dbc55ea450
 
 // Server
 app.listen(port, 'localhost', function () {
